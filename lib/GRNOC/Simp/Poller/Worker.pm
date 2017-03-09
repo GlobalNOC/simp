059b4ce6a876d154f0e95fa22efb40c7299b91d8
Andrew Ragusa <aragusa@io3.bldc.grnoc.iu.edu>
2017-03-09 02:29:12 +0000

    Improved SNMP CollectorImproved SNMP Collector

--- lib/GRNOC/Simp/Poller/Worker.pm
+++ lib/GRNOC/Simp/Poller/Worker.pm
@@ -111,12 +111,6 @@ sub start {
     
     $self->_connect_to_snmp();
 
-    $self->{'purge_timer'} = AnyEvent->timer( after => 1, 
-					      interval => $self->poll_interval * 2,
-					      cb => sub {
-						  $self->_purge_data();
-					      });
-    
     $self->{'collector_timer'} = AnyEvent->timer( after => 1,
 						  interval => $self->poll_interval,
 						  cb => sub {
@@ -127,8 +121,6 @@ sub start {
     #let the magic happen
     AnyEvent->condvar->recv;
 
-    # where the magic happens
-#    return $self->_poll_loop();
 }
 
 sub _poll_cb{
@@ -260,74 +252,6 @@ sub _poll_callback{
 }
 
 
-sub _purge_data{
-  my $self    = shift;
-
-  my $redis   = $self->redis;
-  my $id      = $self->worker_name;  
-
-  $self->logger->error("$id Starting Purge of stale data");
-
-  return if(!defined $self->hosts);
-
-  my @to_be_removed;
-  my @ready_for_delete;
-  foreach my $host(@{$self->hosts}){
-      foreach my $oid (@{$self->oids}){
-	  my $key = $host->{'ip'} . "," . $id . "," . $oid;
-	  
-	  $redis->select(1);
-
-	  while( $redis->llen($key) > $self->retention){
-	      my $ts = $redis->rpop($key);
-	      push(@to_be_removed, $key . ",$ts");
-	  }
-	  
-	  if(defined($host->{'node_name'})){
-	      my $node_key = $host->{'node_name'} . "," . $id . "," . $oid;
-	      
-	      while( $redis->llen($node_key) > $self->retention ){
-		  my $ts = $redis->rpop($node_key);
-		  push(@to_be_removed, $node_key . ",$ts");
-	      }
-	  }	  
-      }
-  }
-  #$self->logger->error("Have a list of stale entries");
-  
-  $redis->select(0);
-  
-  my @possible_oids;
-  #find all the possible OIDs!
-  my $cv = AnyEvent->condvar;
-  foreach my $oid (@{$self->oids}){
-      $cv->begin;
-      my $cursor = 0;
-      while(1){
-	  my $keys;
-	  my $oid_search = $oid . ".*";
-	  #---- get the set of hash entries that match our pattern
-	  my $cv = AnyEvent->condvar;
-	  ($cursor,$keys) = $redis->scan($cursor, MATCH => $oid_search, COUNT => 2000);
-	  foreach my $key (@$keys){
-	      push(@possible_oids,$key);
-	  }
-	  last if($cursor == 0);
-      }
-  }
-
-  my $removed = 0;
-  
-  if(scalar(@to_be_removed) >= 1){
-      foreach my $key (@possible_oids){
-	  my $res = $redis->hdel($key, @to_be_removed);
-	  $removed += $res;
-      }
-  }
-  
-  warn "Total Removed: " . $removed . "\n";  
-}
-
 sub _connect_to_snmp{
     my $self = shift;
     my $hosts = $self->hosts;
@@ -337,15 +261,13 @@ sub _connect_to_snmp{
 	    -hostname         => $host->{'ip'},
 	    -community        => $host->{'community'},
 	    -version          => 'snmpv2c',
+	    -timeout          => $self->snmp_timeout,
 	    -maxmsgsize       => 65535,
 	    -translate        => [-octetstring => 0],
 	    -nonblocking      => 1,
 	    );
 	
 	$self->{'snmp'}{$host->{'ip'}} = $snmp;
-	
-#	my $ip = $host->{'ip'};
-#	$last_seen{$ip} = time;
 	
 	$self->logger->debug($self->worker_name . " assigned host " . $host->{'ip'});
     }
@@ -357,7 +279,7 @@ sub _collect_data{
     my $hosts         = $self->hosts;
     my $oids          = $self->oids;
     my $timestamp     = time;
-    $self->logger->error($self->worker_name. " start poll cycle" );
+    $self->logger->debug($self->worker_name. " start poll cycle" );
 
     for my $host (@$hosts){
 	for my $oid (@$oids){
@@ -380,85 +302,4 @@ sub _collect_data{
     }
 }
 
-sub _poll_loop {
-
-  my ( $self ) = @_;
-
-  my $poll_interval = $self->poll_interval;
-  my $hosts         = $self->hosts;
-  my $oids          = $self->oids;
-
-  my %last_seen;
-
-  #--- if there are no hosts, there is nothing to do
-  if(!defined $hosts || scalar @$hosts == 0){
-    $self->logger->warn($self->worker_name. " no hosts found, nothing to do");
-    return; 
-  }
-
-  foreach my $host(@$hosts){
-     # build the SNMP object for each host of interest
-     my ($snmp, $error) = Net::SNMP->session(
-        -hostname         => $host->{'ip'},
-        -community        => $host->{'community'},
-        -version          => 'snmpv2c',
-        -maxmsgsize       => 65535,
-	 -timeout          => $self->snmp_timeout,
-        -translate        => [-octetstring => 0],
-        -nonblocking      => 1,
-      );
-      $host->{'snmp'} = $snmp; 
-
-      my $ip = $host->{'ip'};
-      $last_seen{$ip} = time;   
-
-      $self->logger->debug($self->worker_name . " assigned host $ip ");
-  }
-
-  #--- on a restart we have lost our in memory cache but the data is still in redis
-  #--- need to perform a scan to rebuild cache
-
-  while (1) {
-    my $timestamp = time;
-    my $waketime = $timestamp + $poll_interval;
-
-    #--- purge data that is older than 2x poll_interval
-    $self->_purge_data();   
-
-    $self->logger->error($self->worker_name. " start poll cycle" ); 
-    for my $host (@$hosts){ 
-      for my $oid (@$oids){
-	 my $reqstr = " $oid -> ".$host->{'ip'};
-	 $self->logger->debug($self->worker_name ." requesting ". $reqstr);
-        #--- iterate through the the provided set of base OIDs to collect
-        my $res =  $host->{'snmp'}->get_table( 
-          -baseoid      => $oid ,
-          -callback     => [\&_poll_callback,$self,$host,$timestamp,\%last_seen,$reqstr, $oid],
-        );
-      } 
-    }
-  
-    #--- go into reactive phase  wait for all results to return or timeouts
-    snmp_dispatcher();
-
-    #--- check to see if we need to restart
-#    if($self->need_restart()){
-#	$self->logger->error("Trying to restart!");
-#      $self->start();
-#    }
-
-    #--- check if we have any hosts that have gone unresponsive
-    foreach my $ip (keys %last_seen){
-      my $lastup = $last_seen{$ip};
-      if($lastup < time - (2 *$poll_interval)){
-        #--- no response in last poll cycle 
-        $self->logger->warn($self->worker_name. " no data from $ip in 3 poll cycles");
-        delete $last_seen{$ip};
-      }
-
-    }
-
-    sleep($waketime - time);
-   } 
-}
 1;
