8ac062df52a193f9fa3320c6a91252c2ba01c3aa
Zachary Catlin <zcatlin@globalnoc.iu.edu>
2017-11-13 20:52:05 +0000

    CompData: _do_functions: added special treatment of 'time' value ISSUE=4618
    'time' gets treated specially by current clients of CompData. I'd already
    plumbed 'time' through _val_cb, but hadn't finished the job in _do_functions.

--- lib/GRNOC/Simp/CompData/Worker.pm
+++ lib/GRNOC/Simp/CompData/Worker.pm
@@ -532,6 +532,17 @@ sub _do_functions{
     my $results      = shift; # request-global $results hash
     my $cv           = shift; # assumes that it's been begin()'ed with a callback
 
+    # First off, by default, we pass through the 'time' value, as it has special
+    # significance for clients:
+    foreach my $host (keys %{$results->{'val'}}){
+        my $val = $results->{'val'}{$host};
+        my $final = $results->{'final'}{$host};
+
+        foreach my $oid_suffix (keys %$val){
+            $final->{$oid_suffix}{'time'} = $val->{$oid_suffix}{'time'};
+        }
+    }
+
     my $xpc = XML::LibXML::XPathContext->new($self->config->{'doc'});
 
     my $vals = $xpc->find("./result/val", $xrefs->get_nodelist);
