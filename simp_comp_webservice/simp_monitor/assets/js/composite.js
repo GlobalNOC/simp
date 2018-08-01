ef3826b188d0eaec79a412f08696750aa06e5e49
Akash Sheth <akashbsheth@gmail.com>
2018-08-01 14:48:52 +0000

    unix timestamp to human readable format change

--- simp_comp_webservice/simp_monitor/assets/js/composite.js
+++ simp_comp_webservice/simp_monitor/assets/js/composite.js
@@ -131,38 +131,3 @@ function getData() {
     myRequest.send();
 }
 
-function convert(p_timestamp){
-
-    // Unixtimestamp
-    // var unixtimestamp = document.getElementById('timestamp').value;
-
-    // Months array
-    var months_arr = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
-
-    // Convert timestamp to milliseconds
-    var date = new Date(p_timestamp*1000);
-
-    // Year
-    var year = date.getFullYear();
-
-    // Month
-    var month = months_arr[date.getMonth()];
-
-    // Day
-    var day = date.getDate();
-
-    // Hours
-    var hours = date.getHours();
-
-    // Minutes
-    var minutes = "0" + date.getMinutes();
-
-    // Seconds
-    var seconds = "0" + date.getSeconds();
-
-    // Display date time in MM-dd-yyyy h:m:s format
-    var convdataTime = month+'-'+day+'-'+year+' '+hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2) + ' --  '+ p_timestamp;
-    return convdataTime;
-    // document.getElementById('datetime').innerHTML = convdataTime;
-
-}
