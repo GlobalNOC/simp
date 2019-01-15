a69eff7257d694097288a2229938246b41c63194
Vincent Orlowski <veorlows@io3.bldc.grnoc.iu.edu>
2019-01-15 16:47:35 +0000

    ISSUE=9449 PROJ=160 Cleaned tabulation to increase readability and added some debugging messages

--- lib/GRNOC/Simp/Poller.pm
+++ lib/GRNOC/Simp/Poller.pm
@@ -122,32 +122,39 @@ sub BUILD {
 
 =cut
 
+
+# Process the config files containing hosts to monitor
+# the configs are located in /etc/simp/hosts.d/ by default
 sub _process_hosts_config{
     my $self = shift;
+    $self->logger->debug("BEGIN processing hosts config");
 
+    # Open hosts.d/ and get array of files within, recursively
     opendir my $dir, $self->hosts_file;
     my @files = readdir $dir;
+    $self->logger->debug("Files in hosts config dir: " . Dumper(\@files));
     closedir $dir;
 
     my @hosts;
 
-    foreach my $file (@files){
+    # Loop through xml host config files and extract hosts
+    foreach my $file (@files) {
 
         next if $file !~ /\.xml$/; # so we don't ingest editor tempfiles, etc.
         
+        $self->logger->debug("Creating host conf: " . $self->hosts_file . "/" . $file);
         my $conf = GRNOC::Config->new( config_file => $self->hosts_file . "/" . $file,
                                        force_array => 1);
-
         
         my $rawhosts = $conf->get("/config/host");
-        foreach my $raw (@$rawhosts){
+        foreach my $raw (@$rawhosts) {
             push(@hosts, $raw);
+            $self->logger->debug("Host \"$raw\" added to \@hosts");
         }
     }
-
-    $self->_set_hosts(\@hosts);
     
-   
+    $self->_set_hosts(\@hosts);
+    $self->logger->debug("FINISHED processing hosts config");
 }
 
 =head2 start
@@ -161,13 +168,12 @@ sub start {
 
     $self->logger->info( 'Starting.' );
 
-    # need to daemonize
     if ( $self->daemonize ) {
-
+    # Daemonized
         $self->logger->debug( 'Daemonizing.' );
 
         my $pid_file = $self->config->get( '/config/@pid-file' )->[0];
-        if(!defined($pid_file)){
+        if ( !defined($pid_file) ) {
             $pid_file = "/var/run/simp_poller.pid";
         }
 
@@ -179,38 +185,35 @@ sub start {
         # jump out of parent
         return if ( $pid );
             
-	$self->logger->debug( 'Created daemon process.' );
+	    $self->logger->debug( 'Created daemon process.' );
 	
-	# change process name
-	$0 = "simpPoller";
+	    # change process name
+	    $0 = "simpPoller";
 	
-	# figure out what user/group (if any) to change to
-	my $user_name  = $self->run_user;
-	my $group_name = $self->run_group;
+	    # figure out what user/group (if any) to change to
+	    my $user_name  = $self->run_user;
+	    my $group_name = $self->run_group;
 	
-	if (defined($group_name)) {
-	    my $gid = getgrnam($group_name);
-	    $self->_log_err_then_exit("Unable to get GID for group '$group_name'") if !defined($gid);
+	    if ( defined($group_name) ) {
+	        my $gid = getgrnam($group_name);
+	        $self->_log_err_then_exit("Unable to get GID for group '$group_name'") if !defined($gid);
 	    
-	    $! = 0;
-	    setgid($gid);
-	    $self->_log_err_then_exit("Unable to set GID to $gid ($group_name)") if $! != 0;
-	}
+	        $! = 0;
+	        setgid($gid);
+	        $self->_log_err_then_exit("Unable to set GID to $gid ($group_name)") if $! != 0;
+	    }
 	
-	if (defined($user_name)) {
-	    my $uid = getpwnam($user_name);
-	    $self->_log_err_then_exit("Unable to get UID for user '$user_name'") if !defined($uid);
+	    if ( defined($user_name) ) {
+	        my $uid = getpwnam($user_name);
+	        $self->_log_err_then_exit("Unable to get UID for user '$user_name'") if !defined($uid);
 	    
-	    $! = 0;
-	    setuid($uid);
-	    $self->_log_err_then_exit("Unable to set UID to $uid ($user_name)") if $! != 0;
-	}
-        
-    }
+	        $! = 0;
+	        setuid($uid);
+	        $self->_log_err_then_exit("Unable to set UID to $uid ($user_name)") if $! != 0;
+	    }
 
-    # dont need to daemonize
-    else {
-
+    } else {
+    # Not Daemonized
         $self->logger->debug( 'Running in foreground.' );
     }
 
@@ -227,19 +230,20 @@ sub start {
     $SIG{'HUP'} = sub {
 
         $self->logger->info( 'Received SIG HUP.' );
-	$self->_set_do_reload(1);
+	    $self->_set_do_reload(1);
         $self->stop();
         # create and store the host portion of the config
     };
 
-    while (1){
-	$self->logger->info( 'Main loop, running process_hosts_config');
+    while (1) {
+	    $self->logger->info( 'Main loop, running process_hosts_config');
         $self->_process_hosts_config();
-	$self->logger->info( 'Main loop, running create_workers');
+
+	    $self->logger->info( 'Main loop, running create_workers');
         $self->_create_workers();
 
-	last if (! $self->do_reload);
-	$self->_set_do_reload(0);
+	    last if (! $self->do_reload);
+	    $self->_set_do_reload(0);
     }
 
     return 1;
@@ -276,6 +280,7 @@ sub stop {
 sub _create_workers {
 
     my ( $self ) = @_;
+    $self->logger->debug("BEGIN creating workers");
 
    #--- get the set of active groups 
     my $groups  = $self->config->get( "/config/group" );
@@ -285,90 +290,93 @@ sub _create_workers {
     my %var_worker;
 
     my $total_workers = 0;
-    foreach my $group (@$groups){
-	#--- ignore the group if it isnt active
-	next if($group->{'active'} == 0);
-	$total_workers += $group->{'workers'};
+    foreach my $group ( @$groups ) {
+	    #--- ignore the group if it isnt active
+	    next if( $group->{'active'} == 0 );
+	    $total_workers += $group->{'workers'};
     }
     
     my $forker = Parallel::ForkManager->new( $total_workers );
 
     #--- create workers for each group
-    foreach my $group (@$groups){
-      #--- ignore the group if it isnt active
-      next if($group->{'active'} == 0);
+    foreach my $group ( @$groups ) {
+        #--- ignore the group if it isnt active
+        next if($group->{'active'} == 0);
+        $self->logger->debug("Creating worker for active group: " . $group->{"name"});
 
-      my $name    = $group->{"name"};
-      my $workers = $group->{'workers'};
+        my $name    = $group->{"name"};
+        my $workers = $group->{'workers'};
 
-      my $poll_interval = $group->{'interval'};
-      my $retention     = $group->{'retention'};
-      my $snmp_timeout  = $group->{'snmp_timeout'};
-      my $max_reps      = $group->{'max_reps'};
+        my $poll_interval = $group->{'interval'};
+        my $retention     = $group->{'retention'};
+        my $snmp_timeout  = $group->{'snmp_timeout'};
+        my $max_reps      = $group->{'max_reps'};
 
-      #--- get the set of OIDS for this group
-      my @oids;
-      foreach my$line(@{$group->{'mib'}}){
-	push(@oids,$line->{'oid'});
-      }
+        #--- get the set of OIDS for this group
+        my @oids;
+        foreach my $line ( @{$group->{'mib'}} ) {
+            push( @oids, $line->{'oid'} );
+            $self->logger->debug("OID Pushed: " . $line->{"oid"});
+        }
 
-      my %hostsByWorker;
-      my %varsByWorker;
-      my $idx=0;
-      #--- get the set of hosts that belong to this group 
-      my $id= $group->{'name'};
+        my %hostsByWorker;
+        my %varsByWorker;
+        my $idx=0;
+        #--- get the set of hosts that belong to this group 
+        my $id= $group->{'name'};
 
-      my @hosts;
+        my @hosts;
       
-      foreach my $host (@{$self->hosts}){
-          my $groups = $host->{'group'};
-          foreach my $group (keys %$groups){
-              if($group eq $id){
-                  #-- match add the host to the host list
-                  push(@hosts,$host);
-                  # no double-pushing:
-                  last;
-              }
-          }
-      }
+        foreach my $host ( @{$self->hosts} ) {
+            my $groups = $host->{'group'};
+            foreach my $group (keys %$groups) {
+                if ($group eq $id) {
+                    #-- match add the host to the host list
+                    push(@hosts,$host);
+                    # no double-pushing:
+                    last;
+                }
+            }
+        }
 
-      #--- split hosts between workers
-      foreach my $host (@hosts){
-	  next if(!defined($host->{'node_name'}));
-	  push(@{$hostsByWorker{$idx}},$host);
-	  if(!$var_worker{$host->{'node_name'}}){
-	      $var_worker{$host->{'node_name'}} = 1;
-	      $varsByWorker{$idx}{$host->{'node_name'}} = 1;
-	  }
-	  $idx++;
-	  if($idx>=$workers) { $idx = 0; }
-      }
-      
-      $self->logger->info( "Creating $workers child processes for group: $name" );     
+        #--- split hosts between workers
+        foreach my $host (@hosts) {
+	        next if ( !defined($host->{'node_name'}) );
 
-      # keep track of children pids
-      $forker->run_on_finish( sub {
-	  
-	  my ( $pid ) = @_;
-	  
-	  $self->logger->error( "Child worker process $pid has died." );	  
-	  
-      });
-      
-      
-      # create workers
-      for (my $worker_id=0; $worker_id<$workers;$worker_id++) {
-	  my $pid = $forker->start();
+	        push(@{$hostsByWorker{$idx}},$host);
 
-	  # We're still in the parent if so
-	  if ($pid){
-	      $self->logger->debug( "Child worker process $pid created." );	      
-	      push( @{$self->children}, $pid );
-	      next;
-	  }
+	        if ( !$var_worker{$host->{'node_name'}} ) {
+	            $var_worker{$host->{'node_name'}} = 1;
+	            $varsByWorker{$idx}{$host->{'node_name'}} = 1;
+	        }
+
+	        $idx++;
+
+	        if($idx>=$workers) { $idx = 0; }
+        }
+      
+        $self->logger->info( "Creating $workers child processes for group: $name" );     
+
+        # keep track of children pids
+        $forker->run_on_finish( sub { 
+	        my ( $pid ) = @_;
+	        $self->logger->error( "Child worker process $pid has died." );	  
+        });
+      
+      
+        # create workers
+        for (my $worker_id=0; $worker_id<$workers; $worker_id++) {
+	        my $pid = $forker->start();
+
+	        # We're still in the parent if so
+	        if ($pid) {
+	            $self->logger->debug( "Child worker process $pid created." );	      
+	            push( @{$self->children}, $pid );
+	            next;
+	        }
 	  
-	  # create worker in this process
-	  my $worker = GRNOC::Simp::Poller::Worker->new( instance      => $worker_id,
+	        # create worker in this process
+	        my $worker = GRNOC::Simp::Poller::Worker->new( instance      => $worker_id,
 							 group_name    => $name,
 							 config        => $self->config,
 							 oids          => \@oids,
@@ -379,16 +387,16 @@ sub _create_workers {
 							 max_reps      => $max_reps,
 							 snmp_timeout  => $snmp_timeout,
 							 var_hosts     => $varsByWorker{$worker_id} || {}
-	      );
+	        );
 	
-        # this should only return if we tell it to stop via TERM signal etc.
-	  $worker->start();
+            # this should only return if we tell it to stop via TERM signal etc.
+            $worker->start();
 	  
-	  $self->logger->info("worker->start has finished for $$");
+	        $self->logger->info("worker->start has finished for $$");
 	  
-	  # exit child process
-	  $forker->finish();
-      }
+	        # exit child process
+	        $forker->finish();
+        }
       
     }
     
@@ -399,6 +407,8 @@ sub _create_workers {
     $self->_set_children( [] );
     
     $self->logger->info( 'All child workers have exited.' );
+
+    $self->logger->debug("FINISHED creating workers");
 }
 
 1;
