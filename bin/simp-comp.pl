7b7947c7a625a9939e4d5f875073ab27fb674845
veorlo <vincentorlowski@gmail.com>
2019-07-08 15:18:09 +0000

    SCTASK0032582 Refactored comp for config redux

--- bin/simp-comp.pl
+++ bin/simp-comp.pl
@@ -4,11 +4,11 @@ use warnings;
 
 #use AnyEvent::Loop;
 use Getopt::Long;
-use GRNOC::Simp::CompData;
+use GRNOC::Simp::Comp;
 
 sub usage {
     my $text = <<"EOM";
-Usage: $0 [--config <file path>] [--logging <file path>]
+Usage: $0 [--config <file path>] [--logging <file path>] [--composites <composites dir>]
     [--nofork] [--user <user name>] [--group <group name>]
 EOM
     print $text;
@@ -16,32 +16,44 @@ EOM
 }
 
 
-use constant DEFAULT_CONFIG_FILE => '/etc/simp/compDataConfig.xml';
-use constant DEFAULT_LOG_FILE    => '/etc/simp/comp_logging.conf';
+use constant DEFAULT_CONFIG_FILE    => '/etc/simp/comp/config.xml';
+use constant DEFAULT_LOG_FILE       => '/etc/simp/comp/logging.conf';
+use constant DEFAULT_COMPOSITES_DIR => '/etc/simp/comp/composites.d/';
+use constant DEFAULT_CONFIG_XSD     => '/etc/simp/comp/validation/config.xsd';
+use constant DEFAULT_COMPOSITE_XSD  => '/etc/simp/comp/validation/composite.xsd';
 
-my $config_file = DEFAULT_CONFIG_FILE;
-my $logging     = DEFAULT_LOG_FILE;
+my $config        = DEFAULT_CONFIG_FILE;
+my $logging       = DEFAULT_LOG_FILE;
+my $composites    = DEFAULT_COMPOSITES_DIR;
+my $config_xsd    = DEFAULT_CONFIG_XSD;
+my $composite_xsd = DEFAULT_COMPOSITE_XSD;
 my $nofork;
 my $help;
 my $username;
 my $groupname;
 
-GetOptions( 'config=s'  => \$config_file,
-            'logging=s' => \$logging,
-            'nofork'    => \$nofork,
-            'user=s'    => \$username,
-            'group=s'   => \$groupname,
-            'help|h|?'  => \$help );
+GetOptions( 
+    'config_file=s'    => \$config,
+    'logging_file=s'   => \$logging,
+    'composites_dir=s' => \$composites,
+    'nofork'           => \$nofork,
+    'user=s'           => \$username,
+    'group=s'          => \$groupname,
+    'help|h|?'         => \$help
+);
 
 usage() if $help;
 
-
-my $data_services = GRNOC::Simp::CompData->new(
-			config_file    => $config_file,
-                        logging_file   => $logging,
-                        run_user       => $username,
-                        run_group      => $groupname,
-			daemonize      => !$nofork );
+my $data_services = GRNOC::Simp::Comp->new(
+    config_file    => $config,
+    logging_file   => $logging,
+    composites_dir => $composites,
+    run_user       => $username,
+    run_group      => $groupname,
+    config_xsd     => $config_xsd,
+    composite_xsd  => $composite_xsd,
+    daemonize      => !$nofork
+);
 
 $data_services->start();
 
