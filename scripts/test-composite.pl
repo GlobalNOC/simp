e4232114ab21c46083f5c5b8a85bda34d07e93da
veorlo <vincentorlowski@gmail.com>
2021-01-12 22:25:18 +0000

    SCTASK0060111 Improved composite test script to only use args as params, no more local editing

--- scripts/test-composite.pl
+++ scripts/test-composite.pl
@@ -12,60 +12,64 @@ use Getopt::Long;
 # Command line usage and parameters
 sub usage {
     my $text = <<"EOM";
-Usage: $0 [--composite <composite_name>]  [--hosts <host names comma-separated] [--iter <iterations>]
+Usage: $0 [--composite|-c <composite_name>]  [--nodes|-n <Node Name(s) Comma-Separated] [--user|-u <RabbitMQ User>] [--pass|-p <RabbitMQ Password>]
 EOM
     print $text;
-    exit( 1 );
+    exit(1);
 }
 
 my $composite;
-my $hosts;
-my $iter;
+my $nodecsv;
+my $user;
+my $pass;
 my $help;
 GetOptions(
     'composite|c=s' => \$composite,
-    'hosts=s'       => \$hosts,
-    'iter|i=s'      => \$iter,
+    'nodes|n=s'     => \$nodecsv,
+    'user|u=s'      => \$user,
+    'pass|p=s'      => \$pass,
     'help|h|?'      => \$help
 ) or usage();
 
 usage() if $help;
 
+# Name of the composite we want
+warn('No composite was specified!') and usage() unless ($composite);
+
+# An array of hosts to request data for
+my @nodes = split(/,/, $nodecsv);
+warn('No host(s) were specified!') and usage() unless (@nodes);
+
+# Credentials for RabbitMQ
+warn('RabbitMQ Username and Password must be specified!') and usage() unless ($user && $pass);
+
 # RabbitMQ Client Setup
 my $client = GRNOC::RabbitMQ::Client->new(
-    host     => "RabbitMQ HOST IP",
+    host     => "127.0.0.1",
     port     => 5672,
-    user     => "RabbitMQ USERNAME",
-    pass     => "RabbitMQ PASSWORD",
+    user     => $user,
+    pass     => $pass,
     exchange => 'Simp',
     timeout  => 60,
     debug    => 1,
     topic    => 'Simp.Comp'
 );
 
-# An array of hosts to request data for
-my @nodes = (
-    'hostname1',
-    'hostname2'
-);
-
 # Continue to request data from Simp.Comp on "interval" seconds
 my $timer = AnyEvent->timer(
     interval => 30,
-    cb       => sub { get(\@nodes); }
+    cb       => sub { get(); }
 );
 
 AnyEvent->condvar->recv;
 
 sub get {
 
-    my $nodes = shift;
-
     # Request data for each node
-    foreach my $node (@{$nodes}) {
+    foreach my $node (@nodes) {
 
         # Send RabbitMQ request for Simp.Comp to compute data for COMPOSITE_NAME
-        my $data = $client->COMPOSITE_NAME(
+        my $data = $client->$composite(
             node           => $node,
             period         => 30,
             async_callback => sub { warn Dumper(shift); }
